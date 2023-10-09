89536e8158042dd23cef354524d08502c80de832
mei30 <meisamjafarimosleh@gmail.com>
2023-10-09 19:39:50 +0330

    Add SEGGER(runtime trace tool)

--- 001Tasks/Core/Src/main.c
+++ 001Tasks/Core/Src/main.c
@@ -22,8 +22,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
-#include "FreeRTOS.h"
-#include "task.h"
+
+#include "../../ThirdParty/FreeRTOS/include/FreeRTOS.h"
+#include "../../ThirdParty/FreeRTOS/include/task.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,6 +46,7 @@ static void task2_handler(void* parameters);
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+#define DWT_CTRL	(*(volatile uint32_t*)0xE0001000)
 
 /* USER CODE END PV */
 
@@ -68,9 +70,9 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 	TaskHandle_t task1_handle;
-		TaskHandle_t task2_handle;
+	TaskHandle_t task2_handle;
 
-		BaseType_t status;
+	BaseType_t status;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -91,17 +93,25 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+
   /* USER CODE BEGIN 2 */
+
+  DWT_CTRL |= (1 << 0);
+
+  SEGGER_SYSVIEW_Conf();
+  SEGGER_SYSVIEW_Start();
+
+
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello world from Task-1", 2, &task1_handle);
 
-    configASSERT(status == pdPASS);
+  configASSERT(status == pdPASS);
 
-    status = xTaskCreate(task2_handler, "Task-2", 200, "Hello world from Task-2", 2, &task2_handle);
+  status = xTaskCreate(task2_handler, "Task-2", 200, "Hello world from Task-2", 2, &task2_handle);
 
-    configASSERT(status == pdPASS);
+  configASSERT(status == pdPASS);
 
-    //start the freeRTOS scheduler
-    vTaskStartScheduler();
+  //start the freeRTOS scheduler
+  vTaskStartScheduler();
 
   /* USER CODE END 2 */
 
@@ -307,23 +317,29 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 static void task1_handler(void* parameters)
 {
+	char msg[100];
 
-	while(1)
-	{
-		printf("%s\n", (char*)parameters);
-		taskYIELD();
-	}
+		while(1)
+		{
+			snprintf(msg,100,"%s\n", (char*)parameters);
+			SEGGER_SYSVIEW_PrintfTarget(msg);
+			taskYIELD();
+		}
 
 }
 
 
 static void task2_handler(void* parameters)
 {
-	while(1)
-	{
-		printf("%s\n", (char*)parameters);
-		taskYIELD();
-	}
+	char msg[100];
+
+			while(1)
+			{
+				snprintf(msg,100,"%s\n", (char*)parameters);
+				SEGGER_SYSVIEW_PrintfTarget(msg);
+				taskYIELD();
+			}
+
 
 }
 /* USER CODE END 4 */
